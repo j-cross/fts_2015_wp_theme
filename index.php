408554031147587553754318c9119d1941da7ee9
Josh Cross <joshcross@ftscreations.com>
2015-02-27 11:13:20 -0600

    Fixed display order issue. Cleaned up footer.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
  get_header();
  $args = array(
                 'post_type' => 'page',
-                'order_by' => 'menu_order',
+                'orderby' => 'menu_order',
                 'order' => 'ASC',
                 'meta_key' => 'fts_2015_one_page',
                 'meta_value' => '1'
@@ -22,7 +22,13 @@
       <span class="page">01</span> / <span class="total-pages"><?php echo $pages_query->post_count ?></span>
     </p>
     <p class="social-icons">
-      <span>Facebook</span>
+      <span><svg version="1.1" id="Your_Icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+   width="100px" height="100px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
+<path d="M58.828,0.05c-30.051,0-27.276,25.117-27.276,25.117H22.46v25.89h9.091V100h29.125V51.057h12.481v-23.27l4.932-23.268
+  C68.534,0.513,58.828,0.05,58.828,0.05z M67.919,18.849c0,0-9.862-1.735-12.175,1.059c-3.396,1.703-2.311,12.657-2.311,12.657H65.76
+  v11.094H53.434v49.159H38.795V43.659h-9.093V32.564c0,0,9.232,0,9.246,0c-0.844-0.002,0.459-10.859,0.558-11.885
+  c0.522-5.447,4.54-10.247,9.708-12.03C56.292,6.21,62.76,7.552,69.768,9.294C69.152,12.478,68.532,15.675,67.919,18.849z"/>
+</svg></span>
       <span>Twitter</span>
       <span>Instagram</span>
     </p>
